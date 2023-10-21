f8864749e459f023c134b8c08c2d8fdc2c7b235e
labit-riset-umm <71822965+alifftwa@users.noreply.github.com>
2023-10-21 10:57:02 +0700

    update boilerplate: add bootstrap & env

--- src/pages/PageContoh/PageContoh.jsx
+++ src/pages/PageContoh/PageContoh.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function PageContoh() {
-    return (
-        <div>
-            Hai dudee!
-        </div>
-    )
+  return (
+    <div>
+      Hai dudee!<br />
+      <button type="button" class="btn btn-primary">
+        Press F5 for Surrend
+      </button>
+    </div>
+  );
 }
 
-export default PageContoh
+export default PageContoh;
