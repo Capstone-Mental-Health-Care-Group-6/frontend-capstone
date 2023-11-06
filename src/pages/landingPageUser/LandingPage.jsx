8d594e3e4726815463770897a3745da27bc746f9
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-11-06 21:06:53 +0700

    responsive home

---
+++ src/pages/landingPageUser/LandingPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import AuthLayout from '../../components/Layout/UserLayout'
+import './LandingPage.style.css'
+import { mentalHealthAmico } from '../../../image'
+
+
+function LandingPage() {
+    return (
+        <AuthLayout>
+            <div className='home' id='home'>
+                <div className="row d-flex align-items-center">
+                    <div className="col d-grid ">
+                        <h1>Apakah Anda <br /> Merasa Stres?</h1>
+                        <p>Temukan solusi terbaik untuk masalah kesehatan mental Anda. Capai kesejahteraan dan kebahagiaan yang Anda inginkan bersama kami.</p>
+                        <button className='btn mt-3' >Mulai Perjalanan Kesehatan Mental Anda</button>
+                    </div>
+                    <div className="col d-flex justify-content-center">
+                        <img src={mentalHealthAmico} alt="" />
+                    </div>
+                </div>
+            </div>
+        </AuthLayout>
+    )
+}
+
+export default LandingPage
