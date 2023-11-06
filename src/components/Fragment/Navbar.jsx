17732287017ab8b24f11d2a7e9ac92bb6d6018ea
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-11-07 06:42:47 +0700

    update responsive home

--- src/components/Fragment/Navbar.jsx
+++ src/components/Fragment/Navbar.jsx
@@ -4,7 +4,7 @@ import { logoEmpathiCare, logoKemenkes } from '../../../image'
 function Navbar({ children }) {
     return (
         <div className='d-flex justify-content-center w-100' >
-            <nav className="navbar navbar-expand-lg position-fixed ">
+            <nav className="navbar navbar-expand-lg position-fixed">
                 <div className="container-fluid">
                     <div className="d-flex align-items-center">
                         <a className="navbar-brand" href="#"><img src={logoEmpathiCare} alt="" /></a>
@@ -12,10 +12,10 @@ function Navbar({ children }) {
                         <span>
                             Diawasi <br /> Oleh
                         </span>
-                        <a className="navbar-brand ms-2" href="#"><img src={logoKemenkes} alt="" /></a>
+                        <a className="navbar-brand kemenkes ms-2" href="#"><img src={logoKemenkes} alt="" /></a>
                     </div>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon "></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav mx-auto">
--- src/pages/landingPageUser/LandingPage.jsx
+++ src/pages/landingPageUser/LandingPage.jsx
@@ -8,9 +8,11 @@ function LandingPage() {
     return (
         <AuthLayout>
             <div className='home' id='home'>
-                <div className="row d-flex align-items-center">
+                <div className="row d-flex align-items-center row-cols-lg-2 row-cols-1">
                     <div className="col d-grid ">
-                        <h1>Apakah Anda <br /> Merasa Stres?</h1>
+                        <h1>Apakah Anda <br />
+                            Merasa Stres?
+                        </h1>
                         <p>Temukan solusi terbaik untuk masalah kesehatan mental Anda. Capai kesejahteraan dan kebahagiaan yang Anda inginkan bersama kami.</p>
                         <button className='btn mt-3' >Mulai Perjalanan Kesehatan Mental Anda</button>
                     </div>
