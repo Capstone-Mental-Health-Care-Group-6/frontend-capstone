7a50ce8d219ba3941768d8e1cfe3486e90bf11ca
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-11-09 07:27:22 +0700

    update button navbar

--- src/components/Fragment/Navbar.jsx
+++ src/components/Fragment/Navbar.jsx
@@ -11,6 +11,13 @@ function Navbar({ children }) {
             setnavbarBg(false)
         }
     }
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleMenuClick = () => {
+        setIsOpen(!isOpen);
+    };
+
     window.addEventListener('scroll', changeBackground)
     return (
         <div className='d-flex justify-content-center w-100' >
@@ -24,9 +31,15 @@ function Navbar({ children }) {
                         </span>
                         <a className="navbar-brand kemenkes ms-2" href="#"><img src={logoKemenkes} alt="" /></a>
                     </div>
-                    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon "></span>
-                    </button>
+                    <div onClick={handleMenuClick} className={`{ navbar-toggler border-0 ${isOpen ? 'opened' : ''}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span>
+                            <svg width="34" height="34" viewBox="0 0 100 100">
+                                <path class="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+                                <path class="line line2" d="M 20,50 H 80" />
+                                <path class="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+                            </svg>
+                        </span>
+                    </div>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav mx-auto">
                             {children}
